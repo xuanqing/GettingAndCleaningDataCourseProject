b86379bafe287608de42c4db267a1ae95d1e8602
xuanQing <xuanqing_liu@hotmail.com>
2014-12-14 21:48:12 +0800

    write the R code and save the tidyset.txt

--- run_analysis.R
+++ run_analysis.R
@@ -8,3 +8,52 @@ testSubjectFile<-'./UCI HAR Dataset/test/subject_test.txt'
 activityLabelFile<-'./UCI HAR Dataset/activity_labels.txt'
 featureFile<-'./UCI HAR Dataset/features.txt'
 
+library(dplyr)
+feature<-read.table(featureFile)
+
+#d1: parse trainset
+trainset<-read.table(trainFile)
+trainlabel<-read.table(trainLabelFile)
+trainsubject<-read.table(trainSubjectFile)
+names(trainset)<-feature[,2]
+names(trainsubject)<-'SubjectID'
+names(trainlabel)<-'ActivityLabel'
+
+#d2: parse testset
+testset<-read.table(testFile)
+testlabel<-read.table(testLabelFile)
+testsubject<-read.table(testSubjectFile)
+names(testset)<-feature[,2]
+names(testsubject)<-'SubjectID'
+names(testlabel)<-'ActivityLabel'
+d1<-cbind(trainsubject,trainlabel,trainset)
+d2<-cbind(testsubject,testlabel,testset)
+
+#t3: combine 2 dataset into 1
+d3<-rbind(d1,d2)
+t3<-tbl_df(d3)
+remove(d1,d2,d3)
+
+#tt4: extract only the subject/label/mean*/std* cols
+tt<-t3[,c(-(303:344),-(382:423),-(461:502))]
+tt1<-select(tt,SubjectID,ActivityLabel)
+tt2<-select(tt,contains('mean()'))
+tt3<-select(tt,contains('std()'))
+tt4<-cbind(tt1,tt2,tt3)
+remove(tt,tt1,tt2,tt3)
+
+#level the subject/activity
+activitylabel<-read.table(activityLabelFile)
+tt4$SubjectID<-factor(tt4$SubjectID,labels='Subject')
+tt4$ActivityLabel<-factor(tt4$ActivityLabel)
+levels(tt4$ActivityLabel)<-activitylabel[[2]]
+
+
+library(tidyr)
+# test for chaining
+tidyset <- 
+    gather(tt4,Measurement,Value,-SubjectID,-ActivityLabel) %>%
+    group_by(SubjectID,ActivityLabel) %>%
+    summarise(mean(Value))
+
+write.table(tidyset,'./tidyset.txt',row.name=FALSE)
