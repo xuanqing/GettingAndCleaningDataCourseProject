2c0b20853ae69b645621ce870ebe052795d74e20
xuanQing <xuanqing_liu@hotmail.com>
2014-12-14 16:37:02 +0800

    add file names

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+trainFile<-'./UCI HAR Dataset/train/X_train.txt'
+trainLabelFile<-'./UCI HAR Dataset/train/y_train.txt'
+trainSubjectFile<-'./UCI HAR Dataset/train/subject_train.txt'
+testFile<-'./UCI HAR Dataset/test/X_test.txt'
+testLabelFile<-'./UCI HAR Dataset/test/y_test.txt'
+testSubjectFile<-'./UCI HAR Dataset/test/subject_test.txt'
+
+activityLabelFile<-'./UCI HAR Dataset/activity_labels.txt'
+featureFile<-'./UCI HAR Dataset/features.txt'
+
